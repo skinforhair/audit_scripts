237bc95de40d30d70b39d906162ba878716df64a
Jeremiah <jeremiahj@nuskin.com>
2015-02-19 15:34:06 -0700

    updated refresh time

--- scripts/runAudit.sh
+++ scripts/runAudit.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 source common/vars
-
+source common/functions
 
 isEmpty=`find $GeneratedScripts -type f -exec echo {} \;`
 
 
 if [ "$isEmpty" != "" ]; then
+	maintenance_page start
 	for i in $GeneratedScripts/*.sh
 	do 
 		$i
 	done
+        maintenance_page stop
 else
 	echo "$GeneratedScripts is empty. Please run readConfigs.sh"
 fi
